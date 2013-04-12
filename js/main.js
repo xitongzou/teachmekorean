daad780e4abf271ea37793df1b84d3e048b2808c
Tong <xitongzou@gmail.com>
2013-04-11 19:05:16 -0700

    added handlebar helpers and backbone views for lesson 1

--- js/main.js
+++ js/main.js
@@ -58,9 +58,56 @@ $(".lesson-2").on('click', function (elem) {
 /** Backbone Models **/
 var HomeContentModel = Backbone.Model.extend({ });
 var homecontent = new HomeContentModel();
-homecontent.set({title: "Teach Me Korean!!", content: "Learn Korean by starting off with simple sentences with mouseovers for each word, and learn to combine words together to create proper sentences and structures.", details: "Learn more &raquo;"});
+homecontent.set({title: "Teach Me Korean!!", 
+content: "Learn Korean by starting off with simple sentences with mouseovers for each word, and learn to combine words together to create proper sentences and structures.",
+ details: "Learn more &raquo;"});
+var Word = Backbone.Model.extend({
+									word: ''
+         });
+var WordGroup = Backbone.Collection.extend({
+                                        model: Word
+});
+var Lesson = Backbone.Model.extend({
+							   enGroup: WordGroup,
+							   krGroup: WordGroup
+         });
+var LessonGroup = Backbone.Collection.extend({ 
+        model: Lesson
+});		 
 
+var Content = Backbone.Model.extend({
+	  heading: "",
+	  content: ""
+});
 
+var LessonContainer = Backbone.Model.extend({
+     title: "",
+	 body: Content,
+	 lessons: LessonGroup
+});
+
+/** Handlebar Helpers **/
+Handlebars.registerHelper('lesson-helper', function(lessonGroup) {
+    var out = "";
+	 
+	 //loop over backbone collection
+	_.each(lessonGroup.models, function(lesson) {
+		out += "<div class='hero-unit'>";
+	   _.each(lesson.get("krGroup").models, function(krWord) {
+	     out += "<h2 class='korean-word'>";
+		 out += krWord.get('word');
+		 out += "</h2>";
+	   });
+	   _.each(lesson.get("enGroup").models, function(enWord) {
+	     out += "<h2 class='english-word'>";
+		 out += enWord.get('word');
+		 out += "</h2>";
+	   });
+		out += "</div";
+	});
+	return out;
+});
+		 
 /** Backbone Views **/ 
 var HomeView = Backbone.View.extend({
                                 initialize: function() {
@@ -73,3 +120,55 @@ var HomeView = Backbone.View.extend({
                                 }
 });
 var homeView = new HomeView({el: $("#header-holder")});
+var Lesson1View = Backbone.View.extend({
+						render: function() {
+						
+						    /** Lesson 1 **/
+							var word1 = new Word();
+							word1.set({ word: "Hello"});
+							var wordGroup1 = new WordGroup();
+							wordGroup1.add(word1);
+							var word2 = new Word();
+							word2.set({ word: "안녕하세요" });
+							var wordGroup2 = new WordGroup();
+							wordGroup2.add(word2);
+							
+							/** Lesson 2 **/
+							var word3 = new Word();
+							word3.set({ word: "Thank You" });
+							var word3a = new Word();
+							word3a.set({ word: "Thanks!!"});
+							var wordGroup3 = new WordGroup();
+							wordGroup3.add(word3);
+							wordGroup3.add(word3a);
+							var word4 = new Word();
+							word4.set({ word: "감사합니다" });
+							var wordGroup4 = new WordGroup();
+							wordGroup4.add(word4);
+							
+							
+							var lesson1 = new Lesson();
+							lesson1.set({enGroup:wordGroup1});
+							lesson1.set({krGroup:wordGroup2});
+							var lesson2 = new Lesson();
+							lesson2.set({enGroup:wordGroup3});
+							lesson2.set({krGroup:wordGroup4});
+							
+							var lessonGroup = new LessonGroup();
+							lessonGroup.add(lesson1);
+							lessonGroup.add(lesson2);
+							
+							var lessonCon = new LessonContainer();
+							lessonCon.set({title:"Lesson 1 - Hello and Thank You",lessons: lessonGroup});
+							var source = $("#lesson-template").html();
+							var template = Handlebars.compile(source);
+                            this.$el.html(template(lessonCon.toJSON()));
+						}
+});
+
+$('.lesson-list').on('click', function(evt) {
+    if (evt.target.className === "lesson-1") {
+	  var lesson1view = new Lesson1View({el: $("#lesson-holder")});
+	  lesson1view.render();
+	}
+});
